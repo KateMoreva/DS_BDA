357ea238a0587c7f42e7ff88ff4acae15fda8d25
Ekaterina Moreva <ekaterina.moreva@vk.team>
2022-12-04 00:10:39 +0300

    frontend

---
+++ src/main/java/ru/spbstu/client/ClientController.java
@@ -0,0 +1,14 @@
+package ru.spbstu.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientController {
+    @GetMapping(path = "/analyse-it-market")
+    public String getWelcomePage(Model model) {
+
+        return "index";
+    }
+}
